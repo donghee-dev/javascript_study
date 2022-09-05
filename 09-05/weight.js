710b7765e1488c6b95615a43b39a00f8c680382d
donghee-dev <dongheedev89@gmail.com>
2022-09-05 12:00:41 +0900

    연산자, 조건문 추가

---
+++ 09-05/weight.js
@@ -0,0 +1,24 @@
+/**
+ * 적정 체중 구하기!!
+ * 
+ * 적정 체중 = (본인 신장 - 100) x 0.9
+ * 
+ * 신장: 180cm
+ * 체중: 80kg
+ * 적정 체중: 72kg
+ * 결과: 적정 체중이 72kg이며, 현재 적성 체중에서 OOkg 초과했습니다.
+ */
+var a = 3;
+var b = 5;
+console.log('적정 체중이', a, 'kg이며, 현재 체중에서', b, 'kg 초과했습니다')
+console.log(`적정 체중이 ${a}kg이며, 현재 체중에서 ${b}kg 초과했습니다`);
+
+
+
+
+
+var height = 180;
+var weight = 74;
+var result = (height - 100) * 0.9;
+var normalWeight = 72;
+console.log(`결과: 적정 체중이 ${normalWeight}이며, 현재 적저`);

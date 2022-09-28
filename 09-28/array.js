13c5765e57f4c242072f995367ef5a00066d20d9
donghee-dev <dongheedev89@gmail.com>
2022-09-28 12:10:41 +0900

    배열, 객체, 함수 학습

---
+++ 09-28/array.js
@@ -0,0 +1,6 @@
+let students = ["문지영", "신현성", "임성민", "류한서"];
+students.push("강유림");
+students.unshift("임다영");
+console.log(students[1]);
+// students.pop();
+// students.shift();
---
+++ 09-28/function.js
@@ -0,0 +1,10 @@
+function plus(num1, num2) {
+    return num1 + num2;
+    // console.log("Plus:", num1 + num2);
+}
+function minus(num1, num2) {
+    return num1 - num2;
+}
+let b = minus(4, 2);
+let a = plus(1, 4) - b;
+let c = plus(2, 10) - b;
